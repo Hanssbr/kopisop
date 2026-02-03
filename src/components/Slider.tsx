066fcb4c881e1845825dec6c9c7eab9bb4d8205f
hanssu <rayhaniqbalbr@gmail.com>
2026-02-03 15:05:53 +0700

    add: marque but need improve

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,11 +1,30 @@
-import React from "react";
-
 export const Slider = () => {
   return (
     <div className="overflow-hidden whitespace-nowrap w-full">
       <div className="flex w-max animate-marquee">
-        <p className="mx-8">COFFEEMU — BEST COFFEE IN TOWN —</p>
-        <p className="mx-8">COFFEEMU — BEST COFFEE IN TOWN —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
+        <p className="mx-8">COFFEEMU —</p>
       </div>
     </div>
   );
