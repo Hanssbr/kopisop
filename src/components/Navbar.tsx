1a716fdc420d985d801f3bce13a76323b03ad9e0
hanssu <rayhaniqbalbr@gmail.com>
2026-02-03 14:37:45 +0700

    fix: mobile size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 export const Navbar = () => {
   return (
     <>
-    <div id='container' className='bg-transparent w-[60%] flex justify-between items-center fixed pt-8'>
-        <div className='logo font-montserrat font-extrabold text-white text-2xl pl-20'>
+    <div id='container' className='bg-transparent w-screen md:w-[60%] flex justify-between items-center fixed pt-8'>
+        <div className='logo font-montserrat font-extrabold text-white text-2xl md:pl-20 pl-5'>
             COFFEEMU
         </div>
-                <div className='menu text-white font-montserrat font-medium text-xl flex justify-between gap-10'>
+                <div id="menu" className='hidden md:flex text-white font-montserrat font-medium text-xl justify-between gap-10'>
                     <a href="#">Home</a>
                     <a href="#">Products</a>
                     <a href="#">Features</a>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,14 +5,31 @@ export const LandingPage = () => {
     <>
       <div id="container">
         <div className="relative text-center">
-          <img
-            src={kopi}
-            alt="kopi"
-            className="md:object-cover w-full object-contain  mix-blend-hard-light"
-          />
-          <div className="w-full absolute top-50 left-0 text-center mt-10">
-            <p className="text-6xl md:text-[260px] font-extrabold mix-blend-plus-lighter opacity-65 text-[#EDECE6] font-montserrat">
+          <div className="w-full h-screen md:h-auto overflow-hidden">
+            <img
+              src={kopi}
+              alt="kopi"
+              className="w-full h-full object-cover md:object-contain mix-blend-hard-light object-center"
+            />
+          </div>
+
+          <div className="md:w-full md:absolute top-50 left-0 text-center mt-10 hidden">
+            <p className="warp-break-word text-9xl md:text-[260px] font-extrabold mix-blend-plus-lighter opacity-65 text-[#EDECE6] font-montserrat">
               COFFEEMU
+            </p>
+          </div>
+          <div className="md:w-full md:hidden absolute top-30 left-0 text-center mt-10">
+            <p className="warp-break-word text-9xl md:text-[260px] font-extrabold mix-blend-plus-lighter opacity-65 text-[#EDECE6] font-montserrat">
+              CO
+            </p>
+            <p className="warp-break-word text-9xl md:text-[260px] font-extrabold mix-blend-plus-lighter opacity-65 text-[#EDECE6] font-montserrat">
+              FF
+            </p>
+            <p className="warp-break-word text-9xl md:text-[260px] font-extrabold mix-blend-plus-lighter opacity-65 text-[#EDECE6] font-montserrat">
+              EE
+            </p>
+            <p className="warp-break-word text-9xl md:text-[260px] font-extrabold mix-blend-plus-lighter opacity-65 text-[#EDECE6] font-montserrat">
+              MU
             </p>
           </div>
         </div>
