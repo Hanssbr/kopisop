756acf86251ee9b02c855169970ff5e9caeaf79f
hanssu <rayhaniqbalbr@gmail.com>
2026-02-03 15:08:06 +0700

    fix: update typo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
     <>
     <div id='container' className='bg-transparent w-screen md:w-[60%] flex justify-between items-center fixed pt-8'>
         <div className='logo font-montserrat font-extrabold text-white text-2xl md:pl-20 pl-5'>
-            COFFEEMU
+            COFFEMU
         </div>
                 <div id="menu" className='hidden md:flex text-white font-montserrat font-medium text-xl justify-between gap-10'>
                     <a href="#">Home</a>
