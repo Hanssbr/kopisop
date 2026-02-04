77f77ef11bfd8d4b73d274247f82a94f4e4494fc
hanssu <rayhaniqbalbr@gmail.com>
2026-02-04 14:47:07 +0700

    change: name for animation slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,7 @@
 export const Slider = () => {
   return (
     <div className="overflow-hidden whitespace-nowrap w-full bg-white">
-      <div className="flex w-max animate-marquee text-6xl text-brown font-extrabold font-montserrat will-change-transform">
+      <div className="flex w-max animate-scroll-side text-6xl text-brown font-extrabold font-montserrat will-change-transform">
         <p className="mx-8">COFFEMU</p>
         <p className="mx-8">COFFEMU</p>
         <p className="mx-8">COFFEMU</p>
